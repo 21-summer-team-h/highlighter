d7cbd40c787a9d1e5820d40314bd3a2d501003ce
newave986 <kingofthefall538@gmail.com>
2021-07-06 18:36:28 +0900

    modify : SECRET_KEY

--- ai/aipart/aipart/settings.py
+++ ai/aipart/aipart/settings.py
@@ -12,21 +12,15 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 
-import os, environ
-env = environ.Env(
-    # set casting, default value
-    DEBUG=(bool, False)
-)
-
+import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = SECRET_KEY = env('SECRET_KEY')
+SECRET_KEY = str(os.getenv('SECRET_KEY'))
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
