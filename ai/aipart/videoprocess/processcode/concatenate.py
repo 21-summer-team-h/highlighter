c2a5344f52e74da1cedcbd3c76d353aa5190d4c6
newave986 <kingofthefall538@gmail.com>
2021-07-07 16:34:04 +0900

    fix : reflect database - concatenate
    have to fix concatenate & cut file
    
    after database

--- ai/aipart/videoprocess/processcode/concatenate.py
+++ ai/aipart/videoprocess/processcode/concatenate.py
@@ -1,17 +1,24 @@
 from moviepy.editor import *
 
-""" 
-path = target path
-"""
+def concatenate_clip(path):
+    tmp = VideoFileClip(path)
+    concatenate = concatenate_videoclips([concatenate, tmp])
+    return concatenate
 
 # path = 
-clip = VideoFileClip(path)
-clip1 = clip.subclip(0, 2)
-clip2 = clip.subclip(5, 7)
-final_clip = concatenate_videoclips([clip1, clip2])
+# clip = VideoFileClip(path)
+# clip1 = clip.subclip(0, 2)
+# clip2 = clip.subclip(5, 7)
+# final_clip = concatenate_videoclips([clip1, clip2])
 
-"""
-path_write = target save path
-"""
+def save_clip(video, save_path):
+    concatenate.write_videofile(save_path)
 
-final_clip.write_videofile(path_write)
+# final_clip.write_videofile(path_write)
+
+for i in range(1, highlight_max+1):
+    path = Address.objects.get(pk=i).highlight_path
+    concatenate = concatenate_clip(path)
+
+save_clip(save_path)
+# set save_path
