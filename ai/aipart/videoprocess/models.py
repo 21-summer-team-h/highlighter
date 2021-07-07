66b58e61674b492318e31a590823b08e779f1b76
newave986 <kingofthefall538@gmail.com>
2021-07-07 15:12:20 +0900

    set : basic set before connecting MySQL

--- ai/aipart/videoprocess/models.py
+++ ai/aipart/videoprocess/models.py
@@ -1,3 +1,24 @@
 from django.db import models
+from django.db import connections
 
-# Create your models here.
+class Video(models.Model):
+    video_index = models.IntegerField(max_length=)
+    video_name = models.CharField(max_length=)
+    video_size = models.CharField(max_length=)
+    video_time = models.CharField(max_length=)
+    video_format = models.CharField(max_length=)
+    video_path = models.CharField(max_length=)
+
+    class Meta:
+        managed = False
+        db_table = 'Video'
+
+class Address(models.Model):
+    video_index = models.IntegerField(max_length=)
+    highlight_index = model.CharField(max_length=)
+    start = model.CharField(max_length=)
+    end = model.CharField(max_length=)
+
+    class Meta:
+        managed = False
+        db_table = 'Address'
--- ai/aipart/videoprocess/views.py
+++ ai/aipart/videoprocess/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
 # Create your views here.
+from .models import Address
+
+def address_view(request):
+    address = Address.objects.all()
+    # return 
