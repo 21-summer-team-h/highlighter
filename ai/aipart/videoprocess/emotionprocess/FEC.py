0734fab49fb6c12d66ae4b5b33688dbdce3e35b1
newave986 <kingofthefall538@gmail.com>
2021-07-13 14:02:53 +0900

    modify : emotion process code to module

--- ai/aipart/videoprocess/emotionprocess/FEC.py
+++ ai/aipart/videoprocess/emotionprocess/FEC.py
@@ -1,7 +1,8 @@
 import numpy as np
 import cv2
+from keras.preprocessing.image import img_to_array
 
-def FER (video, face_cascade):
+def FER (video, face_cascade, emotion_classifier):
 
     emotionResults = np.array([0,0,0,0,0,0,0])
 
@@ -20,11 +21,7 @@ def FER (video, face_cascade):
 
           grayimg = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
-          faces = face_cascade.detectMultiScale(grayimg,
-                                           scaleFactor= 1.1,
-                                           minNeighbors=5,  
-                                           minSize=(20,20)  
-          )
+          faces = face_cascade.detectMultiScale(grayimg, scaleFactor= 1.1, minNeighbors=5, minSize=(20,20))
 
           if len(faces) > 0 :
             face = sorted(faces, reverse=True, key=lambda x: (x[2] - x[0]) * (x[3] - x[1]))[0]
@@ -40,11 +37,13 @@ def FER (video, face_cascade):
             emotionResults[predictions.argmax()] += 1
 
     results = [0,0,0]
+
     for i in range(3):
         label = emotionResults.argmax()
         results[i] = label
         emotionResults[label] = 0
 
     #video.release()
+    print(results)
     return results
 
--- ai/aipart/videoprocess/emotionprocess/get_emotions.py
+++ ai/aipart/videoprocess/emotionprocess/get_emotions.py
@@ -1,7 +1,6 @@
 import cv2
 import numpy as np
 from FEC import FER
-from keras.preprocessing.image import img_to_array
 from keras.models import load_model
 # EMOTIONS = ["Angry","Disgusting","Fearful", "Happy", "Sad", "Surprising", "Neutral"]
 
@@ -11,11 +10,25 @@ from keras.models import load_model
 #   print("Failed to upload video!")
 #   exit()
 
-face_cascade = cv2.CascadeClassifier(r'./haarcascade_frontalface.xml')
-emotion_classifier = load_model(r'./emotion_model.hdf5', compile=False)
+FACE_PATH = #xml file이 있는 path
+CLASS_PATH = #hdf5 file이 있는 path
+VID_PATH = #video file이 있는 path
 
-for i in range(1, highlight_max+1):
-    path = Address.objects.get(pk=i).highlight_path
-    video = cv2.VideoCapture(path)
-    emotion1, emotion2, emotion3 = FER(video, face_cascade)
-    # pk=i인 객체의 emotion_1, emotion_2, emotion_3에 넣는 함수 추가하기
+face_cascade = cv2.CascadeClassifier(FACE_PATH)
+emotion_classifier = load_model(CLASS_PATH, compile=False)
+
+VIDEO_FILE_PATH = VID_PATH
+video = cv2.VideoCapture(VIDEO_FILE_PATH)
+
+if video.isOpened() == False:
+    print("Failed to upload video!")
+    exit()
+
+emotion1, emotion2, emotion3 = FER(video, face_cascade, emotion_classifier)
+print(emotion1, emotion2, emotion3)
+
+# for i in range(1, highlight_max+1):
+#     path = Address.objects.get(pk=i).highlight_path
+#     video = cv2.VideoCapture(path)
+#     emotion1, emotion2, emotion3 = FER(video, face_cascade)
+#     # pk=i인 객체의 emotion_1, emotion_2, emotion_3에 넣는 함수 추가하기
