1a7f5780e7e8ee2b31bb173eca9bd05412749eb7
newave986 <kingofthefall538@gmail.com>
2021-07-13 15:21:46 +0900

    modify : structure changed

--- ai/aipart/videoprocess/views.py
+++ ai/aipart/videoprocess/views.py
@@ -2,5 +2,5 @@ from django.shortcuts import render
 from .models import Video
 
 def video_view(request):
-    videos = Video.objects.all()
+    videos = Video.objects.filter(video_index=1).values("video_name")
     return render(request, 'index.html', {"videos": videos})
