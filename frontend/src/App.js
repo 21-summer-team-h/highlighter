4fea329db0fb3a2ca2166f0fbe8f8d1cf2c24cdb
JerryK026 <kskyung0624@gmail.com>
2021-07-15 14:27:46 +0900

    modify : react -> django video request

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import axios from 'axios';
 
+var fileDownload = require('js-file-download');
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    const handlePDFDownload = () => {
+        axios.get('http://localhost:8000/download', { 
+            responseType: 'blob',
+        }).then(res => {
+            fileDownload(res.data, 'filename.mp4');
+            console.log(res);
+        }).catch(err => {
+            console.log(err);
+        })
+}
+return (
+    <div>
+       <button
+          onClick={() => handlePDFDownload()}>Download File!
+       </button>
     </div>
-  );
+    )
 }
 
 export default App;
