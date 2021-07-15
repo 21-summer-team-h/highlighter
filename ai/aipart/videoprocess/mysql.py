56c698192c2f3007ed22ed3a9458d799e2ced809
Jaehun.Kim <rlawogns0411@naver.com>
2021-07-15 15:23:55 +0900

    fix : fix mysql.py

--- ai/aipart/videoprocess/mysql.py
+++ ai/aipart/videoprocess/mysql.py
@@ -10,10 +10,11 @@ def insert_cutvideo(v_index, h_index, h_path):
     )
     cursor = db.cursor(pymysql.cursors.DictCursor)
     sql = "insert into highlightvideo(video_index, highlight_index, highlight_path) values("+v_index+","+h_index+","+h_path+");"
+    #하이라이트 비디오 테이블에 하이라이트 비디오에 비디오 인덱스, 하이라이트 인덱스, 하이라이트 경로를 가지고 생성
     cursor.execute(sql)
     db.close()
 
-def insert_emotion(v_index, h_index, e1,e2,e3):
+def update_emotion(v_index, h_index, e1,e2,e3):
     db = pymysql.connect(
     user='awfawf',
     passwd='asfafw',
@@ -23,5 +24,20 @@ def insert_emotion(v_index, h_index, e1,e2,e3):
     )
     cursor = db.cursor(pymysql.cursors.DictCursor)
     sql = "update highlightvideo set emotion_1 = "+e1+", emotion_2 = "+e2+", emotion_3 = "+e3+" where video_index = "+v_index+" and highlight_index="+h_index+";"
+    #하이라이트비디오 테이블의 해당 하이라이트 비디오에 emotion들을 추가
+    cursor.execute(sql)
+    db.close()
+
+def update_concatenate(v_index, concatenate_path):
+    db = pymysql.connect(
+    user='awfawf',
+    passwd='asfafw',
+    host='awfawf',
+    db='highlighter',
+    charset='utf8'
+    )
+    cursor = db.cursor(pymysql.cursors.DictCursor)
+    sql = "update video set path = "+concatenate_path+" where video_index = "+v_index+";"
+    #비디오 테이블의 해당 비디오부분의 경로에 완성된 비디오의 경로를 추가  (db수정되면 바꿔야됨)
     cursor.execute(sql)
     db.close()
--- ai/aipart/videoprocess/run.py
+++ ai/aipart/videoprocess/run.py
@@ -16,7 +16,7 @@ for i in range(1, highlight_max+1):
     end = Address.objects.get(pk=i).end
     save_path = Address.objects.get(pk=i).highlight_path
     cut_clip(exist_path, start, end, save_path)
-    mysql.cutvideo(8,i,save_path)   #video_index 8로 하이라이트저장
+    mysql.cutvideo(8,i,save_path)   #video_index 8로 하이라이트번호를 1부터 highlight_max까지 각 경로를 저장
 
 
 
@@ -29,6 +29,7 @@ for i in range(1, highlight_max+1):
     video = cv2.VideoCapture(path)
     emotion1, emotion2, emotion3 = FER(video, face_cascade)
     # pk=i인 객체의 emotion_1, emotion_2, emotion_3에 넣는 함수 추가하기
+    mysql.update_emotion(8,i,emotion1,emotion2,emotion3)#video_index 8로 하이라이트번호를 1부터 highlight_max까지 각 감정을 추가
 
 
 # concatenate
@@ -38,5 +39,6 @@ for i in range(1, highlight_max+1):
     path = Address.objects.get(pk=i).highlight_path
     path_list.append(path_list)
     concatenate = concatenate_clip(path_list)
-
+mysql.update_concatenate(8, save_path) #video_index 8에 만들어진 비디오 경로 추가  
 save_clip(save_path)
+
