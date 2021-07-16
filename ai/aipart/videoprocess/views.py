74a6cca62aa4418a436c51b61cc4cb2d5ef3d731
newave986 <kingofthefall538@gmail.com>
2021-07-16 16:12:13 +0900

    delete : delete ai

--- ai/aipart/videoprocess/views.py
+++
@@ -1,6 +0,0 @@
-from django.shortcuts import render
-from .models import Video
-
-def video_view(request):
-    videos = Video.objects.filter(video_index=1).values("video_name")
-    return render(request, 'index.html', {"videos": videos})
