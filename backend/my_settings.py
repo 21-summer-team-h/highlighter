7b15ff21548d219c2d4dd873c7003c69a6eb8743
SeokyungKim <kskyung0624@gmail.com>
2021-07-16 13:49:43 +0900

    modify : reordered backend files

---
+++ backend/my_settings.py
@@ -0,0 +1,10 @@
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'test',
+        'USER': 'root',
+        'PASSWORD': 'password',
+        'HOST': 'localhost',
+        'PORT': '', 
+    }
+}
