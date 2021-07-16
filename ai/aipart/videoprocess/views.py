5d1b566f54961bf61e26d65773264e5342398c6e
Jaehun.Kim <rlawogns0411@naver.com>
2021-07-16 15:38:46 +0900

    delete : delete ai

--- ai/aipart/videoprocess/views.py
+++
@@ -1,9 +0,0 @@
-from django.shortcuts import render
-
-# Create your views here.
-from .models import Address
-
-def address_view(request):
-    address = Address.objects.all()
-    return address
-    # return render(request, {"address": address})
