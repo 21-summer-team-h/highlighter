297e618aa5f84467fd1dd4a2199319576379841c
cje1903 <cje1396@gmail.com>
2021-07-23 13:30:05 +0900

    add: showReulst.css

--- frontend/src/Components/Views/showResult.js
+++ frontend/src/Components/Views/showResult.js
@@ -3,6 +3,10 @@ import { Route, Link } from "react-router-dom";
 import './showResult.css';
 import axios from "axios";
 import logoImg from "../images/logo.png";
+import Facebook_logo from "../images/facebook-logo.png";
+import Youtube_logo from "../images/youtube-logo.png";
+import Twitter_logo from "../images/twitter-logo.png";
+import Instagram_logo from "../images/instagram-logo.png";
 
 
 const ShowResult = (props) => {
@@ -62,11 +66,20 @@ const ShowResult = (props) => {
             <hr id="line2"></hr>
             <img id="logo" src={logoImg}/>
 
+            <div class="leftbar">
+                <div class="folder"><span>Upload & Share</span></div>
+                <div class="folder"><img id = "folderImg" src={ Facebook_logo }/><span>Facebook</span></div>
+                <div class="folder"><img id = "folderImg" src={ Twitter_logo }/><span>Twitter</span></div>
+                <div class="folder"><img id = "folderImg" src={ Instagram_logo }/><span>Instagram</span></div>
+                <div class="folder"><img id = "folderImg" src={ Youtube_logo }/><span>Youtube</span></div>
+            </div>
+
+            <div id="videoBox"></div>
             {/* 완성 영상 재생시키기
             <video id="show_video" width="2600" height="2000" src={}} controls></video>
             controls이 존재하면, 소리 조절(volume), 동영상 탐색(seek), 일시 정지(pause)/재시작(resume)을 할 수 있는 컨트롤러를 제공합니다.*/}
 
-            <div id="videoBox"></div>
+
             {/* 다운로드 버튼 구현 */}
             <button id="downloadButton" class="btn" /* onclick={ handleDownload } */ >Download</button>
 
